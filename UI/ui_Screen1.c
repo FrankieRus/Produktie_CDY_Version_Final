082740054cb90cfc6cd2b05b21da4999006f14ef
Frankier <rustigefrank@gmail.com>
2025-08-02 22:52:37 +0200

    Font 36

--- UI/ui_Screen1.c
+++ UI/ui_Screen1.c
@@ -102,7 +102,7 @@ void ui_Screen1_screen_init(void)
     lv_obj_set_y(ui_version, -39);
     lv_obj_set_align(ui_version, LV_ALIGN_CENTER);
     lv_label_set_text(ui_version, "............................");
-    lv_obj_set_style_text_font(ui_version, &lv_font_montserrat_42, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_font(ui_version, &lv_font_montserrat_36, LV_PART_MAIN | LV_STATE_DEFAULT);
 
     ui_Aanvoer = lv_label_create(ui_TabPage1);
     lv_obj_set_width(ui_Aanvoer, LV_SIZE_CONTENT);   /// 1
